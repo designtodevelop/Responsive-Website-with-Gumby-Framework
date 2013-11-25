25dacf09b0392f6f070579b185c43ec1195aafa0
Steven Wu <steven@cyber-blog.com>
2013-11-25 10:05:45 +0000

    Building Landing Page

--- js/custom-ck.js
+++
@@ -1,1 +0,0 @@
-Gumby.ready(function(){$(".contact-form form").validation({required:[{name:"name"},{name:"email",validate:function(e){return e.val().indexOf("@")>-1}},{name:"message"}],submit:function(e){Gumby.debug(e);alert("Your Message Has Been Sent")},fail:function(){alert("Ooops looks like there's an error.")}})});
---
+++ js/main.js
@@ -0,0 +1,29 @@
+Gumby.ready(function() { 
+    // Contact Form Validation
+    $('.contact-form form').validation({
+        required: [
+            {
+                name: 'name'
+            },
+            {
+                name: 'email',
+                // check this email has a @
+                validate: function($el) {
+                        return $el.val().indexOf('@') > -1;
+                }
+            },
+            {
+              name: 'message',
+            }
+        ],
+        // Validation Successful Message
+        submit: function(formData) {
+                Gumby.debug(formData);
+                alert("Your Message Has Been Sent");
+        },
+        // Validation has failed message
+        fail: function() {
+                alert("Ooops looks like there's an error.");
+        }
+    });
+});
